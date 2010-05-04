be0eaca0a2c566741d9b7e11a72ebacecce04872
Jeremy Durham <jeremydurham@gmail.com>
2010-05-04 00:52:39 -0400

    Updated version to 1.0.1

--- lib/seed.rb
+++ lib/seed.rb
@@ -1,5 +1,5 @@
 class Seed
-  VERSION = '1.0'
+  VERSION = '1.0.1'
   
   def self.plant(klass, name, attributes={}, &block)    
     raise RuntimeError, "You cannot overwrite an existing seed" if self.planted?(klass, name)
