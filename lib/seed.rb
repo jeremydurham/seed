b768f381d91e1653586ece3143fba7306ce5ed8f
Jeremy Durham <jeremydurham@gmail.com>
2010-11-24 23:23:58 -0500

    Added MissingRowError, MissingSeedError, and SeedOverwriteError (to replace RuntimeError)

--- lib/seed.rb
+++ lib/seed.rb
@@ -1,3 +1,7 @@
 require File.join(File.dirname(__FILE__), 'seed', 'railtie') if defined?(Rails)
 require File.join(File.dirname(__FILE__), 'seed', 'base')
 require File.join(File.dirname(__FILE__), 'seed', 'version')
+
+class MissingRowError  < StandardError; end
+class MissingSeedError < StandardError; end
+class SeedOverwriteError < StandardError; end
